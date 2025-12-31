e1fd44647de7a50f907fbe9e42bc6ba2168f3c47
Eduardo Faleiro <eduardoviniciusfaleiro@gmail.com>
2025-12-31 16:11:55 -0300

    feat: init db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,41 @@
-package api
+package main
+
+import (
+	"database/sql"
+	"os"
+
+	"github.com/eduardovfaleiro/colabtask/internal/handler"
+	"github.com/eduardovfaleiro/colabtask/internal/repository"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+
+	if dbURL == "" {
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+
+	if err != nil {
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+	}
+	taskRepo := repository.NewPostgreTaskRepository(db)
+	taskHandler := handler.NewTaskHandler(taskRepo)
+
+	projectRepo := repository.NewPostgreProjectRepository(db)
+	projectHandler := handler.NewProjectHandler(projectRepo)
+
+	r := gin.Default()
+
+	r.POST("/tasks", taskHandler.Create)
+	r.POST("/projects", projectHandler.Create)
+	r.Run(":8080")
+}
